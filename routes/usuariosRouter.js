cb4cf126dc7dd05813db16ba8cc144a29bc8eeee
Jers3084 <jericrdez.c03@ucamp.utel.edu.mx>
2022-04-06 21:14:26 -0500

    Update
    Se agrego validación de campos del Registro de Usuarios

--- routes/usuariosRouter.js
+++ routes/usuariosRouter.js
@@ -2,9 +2,10 @@ const { Router } = require("express");
 const { listarUsuarios, guardarNuevoUsuario, actualizarUsuarioC, actualizaPassword, login } = require("../controller/usuariosController");
 const { validateToken } = require ('../middleware/validateTokenMiddleware')
 const router = Router();
+const validator = require("../validators/userValidator");
 
 router.get("/", validateToken, listarUsuarios);
-router.post("/", guardarNuevoUsuario);
+router.post("/", validator.userCreateValidator, guardarNuevoUsuario);
 router.post("/actualizar",validateToken, actualizarUsuarioC);
 router.post("/actpassword", validateToken, actualizaPassword);
 router.post("/login", login);
--- validators/userValidator.js
+++ validators/userValidator.js
@@ -6,24 +6,19 @@ exports.userCreateValidator = [
    .withMessage("El campo nombre es requerido")
    .isString()
    .withMessage("El campo nombre es de tipo string"),
-   check("apellido")
-   .notEmpty()
-   .withMessage("El campo apellido es requerido")
-   .isString()
-   .withMessage("El campo apellido es de tipo string"),
    check("email")
    .notEmpty()
    .withMessage("El campo email es requerido")
    .isEmail()
    .withMessage("El campo email debe ser un correovalido"),
+   check("username")
+   .notEmpty()
+   .withMessage("El campo username es requerido")
+   .isString()
+   .withMessage("El campo username es de tipo string"),
    check("password")
    .notEmpty()
    .withMessage("El campo password es requerido")
    .isString()
    .withMessage("El campo password es de tipo string"),
-   check("rol")
-   .notEmpty()
-   .withMessage("El campo rol es requerido")
-   .isArray()
-   .withMessage("El campo rol es de tipo array"),
 ];
